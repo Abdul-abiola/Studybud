ff3960b2c7a9430237d0a00be935eeee3469908b
Abdul-abiola <abdulrahamanabdulrahaman03@gmail.com>
2026-05-30 08:00:46 -0700

    Update settings.py

--- STUDYBUD/settings.py
+++ STUDYBUD/settings.py
@@ -129,9 +129,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "default":{
+        "Backend": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
