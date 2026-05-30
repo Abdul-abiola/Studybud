1119b301c5e51ef99038f9eb7e7f0b0d427a58f2
Abdul-abiola <abdulrahamanabdulrahaman03@gmail.com>
2026-05-30 06:48:50 -0700

    Update settings.py

--- STUDYBUD/settings.py
+++ STUDYBUD/settings.py
@@ -49,9 +49,10 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'base.User'
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
     
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
